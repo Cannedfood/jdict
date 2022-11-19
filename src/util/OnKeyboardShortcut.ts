7bfbab7f5025be693f185094453761770f59512d
Benno Straub <benno.straub@outlook.de>
2022-11-19 17:29:48 +0100

    Added modifier support to useKeyboardShortcuts

--- src/util/OnKeyboardShortcut.ts
+++ src/util/OnKeyboardShortcut.ts
@@ -1,31 +1,48 @@
 import { onMounted, onUnmounted } from "vue";
 
+function parseShortcut(shortcut: string) {
+	const keys = shortcut.toLowerCase().split("+");
+	return {
+		key: keys.pop()!,
+		ctrl: keys.includes('ctrl'),
+		shift: keys.includes('shift'),
+		alt: keys.includes('alt'),
+	};
+}
+
 export function onKeyboardShortcut(desc: { [key: string]: () => boolean|void }) {
-    const cb = (e: KeyboardEvent) => {
-        for(const [k, v] of Object.entries(desc)) {
-            for(const exp of k.split(',').map(e => e.trim())) {
-                if(exp.toLowerCase() === e.key.toLowerCase()) {
-                    if(v() !== false) {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        return;
-                    }
-                }
-            }
-        }
-    };
-    const self = {
-        subscribed: false,
-        subscribe: () => {
-            if(!self.subscribed)
-                document.addEventListener('keydown', cb);
-        },
-        unsubscribe: () => {
-            if(self.subscribed)
-                document.removeEventListener('keydown', cb);
-        }
-    }
-    onMounted(() => self.subscribe());
-    onUnmounted(() => self.unsubscribe());
-    return self;
+	const cb = (e: KeyboardEvent) => {
+		for(const [k, v] of Object.entries(desc)) {
+			for(const exp of k.split(',').map(e => e.trim())) {
+				const { key, ctrl, shift, alt } = parseShortcut(exp);
+				if(
+					(key === e.key.toLowerCase()) &&
+					(!ctrl || e.ctrlKey) &&
+					(!shift || e.shiftKey) &&
+					(!alt || e.altKey)
+				)
+				{
+					if(v() !== false) {
+						e.preventDefault();
+						e.stopPropagation();
+						return;
+					}
+				}
+			}
+		}
+	};
+	const self = {
+		subscribed: false,
+		subscribe: () => {
+			if(!self.subscribed)
+				document.addEventListener('keydown', cb);
+		},
+		unsubscribe: () => {
+			if(self.subscribed)
+				document.removeEventListener('keydown', cb);
+		}
+	}
+	onMounted(() => self.subscribe());
+	onUnmounted(() => self.unsubscribe());
+	return self;
 }
