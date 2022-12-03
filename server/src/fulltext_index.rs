5e8665eae58ad8f8b25943b13c0c1433a09bc959
Benno Straub <benno.straub@outlook.de>
2022-12-03 20:54:01 +0100

    Fixed FullTextIndex for kanji

--- server/src/fulltext_index.rs
+++ server/src/fulltext_index.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
 
 use itertools::Itertools;
+use unicode_blocks::{find_unicode_block, is_cjk_block};
+
+pub type Syllable = [char; 3];
 
 pub struct FullTextIndex {
-    pub entries: HashMap<[char; 3], Vec<u32>>,
+    pub entries: HashMap<Syllable, Vec<u32>>,
 }
 
 impl FullTextIndex {
@@ -16,16 +19,15 @@ impl FullTextIndex {
     // pub fn insert_many<'a, I>(&mut self, texts: I, id: u32)
     //     where I: Iterator<Item = &'a str>
     // {
-    //     let tuples = texts.flat_map(|s| s.chars().tuple_windows::<(char, char, char)>());
+    //     let tuples = texts.flat_map(|s| s.chars().tuple_windows::<Syllable>());
     //     for (a, b, c) in tuples {
     //         self.entries.entry([a, b, c]).or_insert(vec![]).push(id);
     //     }
     // }
 
     pub fn insert(&mut self, text: &str, id: u32) {
-        let tuples = text.chars().tuple_windows::<(char, char, char)>();
-        for (a, b, c) in tuples {
-            self.entries.entry([a, b, c]).or_insert(vec![]).push(id);
+        for syllable in syllables(text) {
+            self.entries.entry(syllable).or_insert(vec![]).push(id);
         }
     }
 
@@ -37,11 +39,38 @@ impl FullTextIndex {
     }
 
     pub fn broadphase_search(&self, text: &str) -> Option<Vec<u32>> {
-        text.chars()
-        .tuple_windows()
+        syllables(text)
         .unique()
-        .filter_map(|(a, b, c)| self.entries.get(&[a, b, c]))
+        .filter_map(|syllable| self.entries.get(&syllable))
         .min_by_key(|ids| ids.len())
         .cloned()
     }
+}
+
+fn first_syllable(s: &str) -> Syllable {
+    let mut chars = s.chars();
+    let a = chars.next().unwrap_or('\0');
+    let b = chars.next().unwrap_or('\0');
+    let c = chars.next().unwrap_or('\0');
+
+    let a_codeblock = find_unicode_block(a).unwrap();
+    let b_codeblock = find_unicode_block(b).unwrap();
+    let c_codeblock = find_unicode_block(c).unwrap();
+
+    if is_cjk_block(a_codeblock) || a_codeblock != b_codeblock {
+        [a, '\0', '\0']
+    }
+    else if a_codeblock != c_codeblock {
+        [a, b, '\0']
+    }
+    else {
+        [a, b, c]
+    }
+}
+
+fn syllables<'a>(s: &'a str) -> impl Iterator<Item = Syllable> + 'a {
+    s.char_indices()
+    .map(
+        |c| first_syllable(&s[c.0..])
+    )
 }
