3060be7961b3544df92d9bcde3a523ba4aa97524
Benno Straub <benno.straub@outlook.de>
2023-01-10 14:29:44 +0100

    Fixed tauri file handling

--- code/tauri/src/main.rs
+++ code/tauri/src/main.rs
@@ -5,6 +5,7 @@
 
 use jdict_db::database::Config;
 use jdict_db::database::Database;
+use tauri::Manager;
 
 #[tauri::command]
 fn search<'a>(search_term: &str, take: Option<u32>, skip: Option<u32>, db: tauri::State<Database>) -> jdict_db::shared_api::SearchResult {
@@ -12,16 +13,25 @@ fn search<'a>(search_term: &str, take: Option<u32>, skip: Option<u32>, db: tauri
 }
 
 fn main() {
-    let database = Database::new(
-        Config {
-            jmdict_file: "res/JMdict_e.gz".to_string(),
-            kanjidic_file: "res/kanjidic2.xml.gz".to_string(),
-            kanjivg_file: "res/kanjivg.xml.gz".to_string(),
-        }
-    );
 
     tauri::Builder::default()
-        .manage(database)
+        .setup(|app| {
+            let resolve = |path: &str| {
+                app.path_resolver()
+                .resolve_resource(path).unwrap()
+                .to_str().unwrap()
+                .to_string()
+            };
+
+            app.manage(Database::new(
+                Config {
+                    jmdict_file: resolve("../../res/JMdict_e.gz"),
+                    kanjidic_file: resolve("../../res/kanjidic2.xml.gz"),
+                    kanjivg_file: resolve("../../res/kanjivg.xml.gz"),
+                }
+            ));
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![search])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
