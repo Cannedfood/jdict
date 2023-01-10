dede9bf7e054daa2e709ca83f068a7c4226e68af
Benno Straub <benno.straub@outlook.de>
2023-01-10 08:50:02 +0100

    Replaced kanji-info expand button with chevron

--- code/web/src/components/KanjiInfo.vue
+++ code/web/src/components/KanjiInfo.vue
@@ -18,51 +18,55 @@ const expanded = ref(props.startExpanded ?? false);
 
 <template lang="pug">
 .kanji-info(:class="{expanded}" @click="!expanded && (expanded = true)")
-	.kanji {{kanji.literal}}
-	.tags
-		.strokes(v-if="expanded && kanjivg")
-			StrokeOrder(:kanjivg="kanjivg")
-		.pill(v-if="kanji.misc.jlpt") JLPT {{kanji.misc.jlpt}}
-		.pill(v-if="kanji.misc.grade") Grade {{kanji.misc.grade}}
-		.pill(v-if="kanji.misc.freq") Freq: {{kanji.misc.freq}}
-		.pill(v-if="kanji.misc.stroke_count && kanji.misc.stroke_count.length > 0")
-			| Strokes: {{kanji.misc.stroke_count[0]}}
-			//- span(v-if="kanji.stroke_count.length > 1")
-			//- 	| ({{kanji.stroke_count.slice(1).join(', ')}})
+	.left
+		.kanji {{kanji.literal}}
+		.tags
+			.strokes(v-if="expanded && kanjivg")
+				StrokeOrder(:kanjivg="kanjivg")
+			.pill(v-if="kanji.misc.jlpt") JLPT {{kanji.misc.jlpt}}
+			.pill(v-if="kanji.misc.grade") Grade {{kanji.misc.grade}}
+			.pill(v-if="kanji.misc.freq") Freq: {{kanji.misc.freq}}
+			.pill(v-if="kanji.misc.stroke_count && kanji.misc.stroke_count.length > 0")
+				| Strokes: {{kanji.misc.stroke_count[0]}}
+				span(v-if="kanji.misc.stroke_count.length > 1")
+					| ({{kanji.misc.stroke_count.slice(1).join(', ')}})
 	.info
 		KanjiReadings(:kanji="kanji" :expanded="expanded")
 		.radical(v-if="kanji.misc.rad_name?.length") Radical: {{kanji.misc.rad_name.join(', ')}}
 		.decomposition(v-if="expanded && kanjivg")
 			h5 Decomposition:
 			KanjiDecomposition(:kanjivg="kanjivg")
-	.expand-btn(role="button" @click.stop="expanded = !expanded")
-		| {{expanded? 'Less' : 'More'}}
+	.more(@click.stop="expanded = !expanded")
+		.chevron(:class="{ up: expanded }") ❯
 </template>
 
 <style lang="scss" scoped>
 .kanji-info {
-	display: grid;
-	grid-template-columns: min-content;
-	grid-template-rows: min-content;
-	grid-template-areas:
-		"kanji info"
-		"kanji info"
-		"tags  info"
-		"tags  bonus"
-		"more  more";
-
+	// Styling
 	border: 1px solid #888;
 	border-radius: .5em;
 	margin-block: .5em;
 	padding: .5em;
 	&>* { margin: .5rem; }
 
-	cursor: pointer;
-	&.expanded { cursor: unset; }
+	&:not(.expanded) {
+		cursor: pointer;
+		// box-shadow: inset 0 -2em 2em -2em #000;
+	}
 
+	// Layout
+	display: grid;
+	grid-template-columns: min-content auto;
+	grid-template-rows: 1fr 2em;
+	grid-template-areas:
+		"left info"
+		"more more";
+	.left { grid-area: left; }
+	.info { grid-area: info; }
+	.more { grid-area: more; }
+
+	// Children
 	.kanji {
-		grid-area: kanji;
-
 		text-align: center;
 		font-size: 3.5em;
 		width: 6rem;
@@ -72,15 +76,12 @@ const expanded = ref(props.startExpanded ?? false);
 		border-radius: .2rem;
 	}
 	.tags {
-		grid-area: tags;
 		font-size: .8em;
+		margin-top: 1em;
 	}
-	.info {
-		grid-area: info;
-	}
-
-	.expand-button {
-		grid-area: more;
+	.more {
+		text-align: center;
+		cursor: pointer;
 	}
 }
 </style>
